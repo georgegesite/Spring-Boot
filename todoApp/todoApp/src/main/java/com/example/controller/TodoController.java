1e09eb067e4f567c3f96f6abef26031ce1a9f62f
George Gesite <georgesite33@gmail.com>
2023-11-23 20:35:12 +0800

    Todos
    Read Created
    -added Bootstrap Static Files
    -added templates for CRUD

--- todoApp/todoApp/src/main/java/com/example/controller/TodoController.java
+++ todoApp/todoApp/src/main/java/com/example/controller/TodoController.java
@@ -1,15 +1,28 @@
 package com.example.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.dto.TodoDtoEntity;
 import com.example.services.TodoServices;
 
 @Controller
 public class TodoController {
 
-	private TodoServices todoService;
-	
-	public TodoController(TodoServices todoService) {
-		this.todoService = todoService;
-	}
+    private TodoServices todoService;
+
+    public TodoController(TodoServices todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping("/todos")
+    public String listClubs(Model model) {
+        List<TodoDtoEntity> todos = todoService.findAllClubs();
+        model.addAttribute("todos", todos);
+        return "todos-list";
+    }
+
 }
--- todoApp/todoApp/src/main/java/com/example/services/TodoServices.java
+++ todoApp/todoApp/src/main/java/com/example/services/TodoServices.java
@@ -1,15 +1,36 @@
 package com.example.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import com.example.dto.TodoDtoEntity;
 import com.example.repository.TodoRepository;
+import com.example.entity.TodoEntity;
 
 @Service
 public class TodoServices {
 
-	private TodoRepository todoRepository;
-	
-	public TodoServices (TodoRepository todoRepository){
-		this.todoRepository = todoRepository;
-	}
+    private TodoRepository todoRepository;
+
+    public TodoServices(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<TodoDtoEntity> findAllClubs() {
+        List<TodoEntity> todos = todoRepository.findAll();
+        return todos.stream().map((todo) -> mapToTodoDto(todo)).collect(Collectors.toList());
+
+    }
+
+    private TodoDtoEntity mapToTodoDto(TodoEntity todo) {
+        TodoDtoEntity todoDto = TodoDtoEntity.builder()
+                .id(todo.getId())
+                .title(todo.getTitle())
+                .content(todo.getContent())
+                .build();
+
+        return todoDto;
+    }
 }
