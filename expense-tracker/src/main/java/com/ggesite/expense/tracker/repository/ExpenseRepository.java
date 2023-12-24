a923de5694173bb187a658e9f7837b7c5b4dd793
George Gesite <georgesite33@gmail.com>
2023-12-24 19:25:21 +0800

    update

--- expense-tracker/src/main/java/com/ggesite/expense/tracker/repository/ExpenseRepository.java
+++ expense-tracker/src/main/java/com/ggesite/expense/tracker/repository/ExpenseRepository.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ggesite.expense.tracker.entity.ExpenseEntity;
 
@@ -14,4 +15,8 @@ public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
 
     @Query("SELECT COALESCE(SUM(e.amount), 0) FROM ExpenseEntity e WHERE DATE(e.createdOn) = CURRENT_DATE")
     BigDecimal sumAmountForToday();
+
+    @Query("SELECT e.category, SUM(e.amount) FROM ExpenseEntity e WHERE MONTH(e.createdOn) = :month GROUP BY e.category")
+    List<Object[]> sumAmountPerCategoryPerMonth(@Param("month") int month);
+
 }
